47b851bb93c56e3f8fd15fedc452bb3760fdbd47
agosgaggioli <gaggioliagostina@gmail.com>
2025-02-28 18:30:26 -0300

    js listo

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-class Actividad{
+class Activity{
     constructor(id,title, descripcion , urlimg){
         this.id=id
         this.title=title
@@ -7,22 +7,103 @@ class Actividad{
     }
 }
 
-class Repositorio {
+class Repository {
     constructor(){
-        this.actividades=[];
+        this.activities=[];
+        this.count=0;
+
     }
     getAllActivities(){
-        return this.actividades
+        return this.activities
     }
-    createActivity(id, title, descripcion, urlimg){
-        const actividad = new Actividad(id,title, descripcion, urlimg)
-        this.actividades.push(actividad)
-        return this.actividades
+    createActivity(title, descripcion, urlimg){
+        
+        const actividad = new Activity(this.count,title, descripcion, urlimg)
+        this.activities.push(actividad)
+        this.count++;
+       
+        this.getAllActivities()
     }
     deleteActivity(id){
-        this.actividades =this.actividades.filter( actividades=> actividades.id !== id)
-        return this.actividades
+        this.activities = this.activities.filter( activity=> activity.id !== id)
+        this.getAllActivities()
     }
 
 }
+const repository = new Repository()
 
+
+const createActivityCard=(activity)=>{
+
+    const{id,title, descripcion, urlimg}= activity
+
+    const carta = document.createElement('div')
+    const titulocard = document.createElement ('h3')
+    const descripcioncard = document.createElement('p')
+    const urlimgcard =document.createElement('img')
+    const deleteBtn=document.createElement('button')
+
+    carta.id= id
+    titulocard.innerHTML=title
+    descripcioncard.innerHTML=descripcion
+    urlimgcard.src=urlimg
+    urlimgcard.alt= title
+    deleteBtn.innerText= "Delete Activity"
+
+    carta.classList.add('cartas-act')
+    
+    deleteBtn.addEventListener('click',()=>{
+        repository.deleteActivity(id)
+        reanderactivities()
+    })
+
+
+    
+
+    carta.appendChild(titulocard)
+    carta.appendChild(descripcioncard)
+    carta.appendChild(urlimgcard)
+    carta.appendChild(deleteBtn)
+    
+
+    return carta
+
+
+}
+const reanderactivities=()=>{
+    const contenedor = document.getElementById('contenedor-actividades')
+
+    contenedor.innerHTML=""
+
+    const actividades = repository.getAllActivities().map((createActivityCard));
+
+    
+    actividades.forEach(actividad => contenedor.appendChild(actividad));
+    
+    console.log(contenedor)
+
+}
+
+
+
+const formm = document.getElementById('form')
+const handler =(event)=>{
+    event.preventDefault()
+    
+        const title= document.getElementById('titulo').value.trim()
+        const descripcion= document.getElementById('descripcion').value.trim()
+        const urlimg= document.getElementById('url').value.trim()
+    
+        
+        if (!title || !descripcion || !urlimg){
+            alert( "debe completar los campos")
+            return
+        }
+    repository.createActivity(title, descripcion, urlimg)
+    reanderactivities ()
+    formm.reset();
+ 
+}
+
+
+formm.addEventListener("submit", handler)
